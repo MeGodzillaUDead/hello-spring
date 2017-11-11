505cf45031b6941ca09e31c92b4d7f273ff5dda9
Jonathan Riezman <riezman@gmail.com>
2017-11-11 00:05:29 -0600

    class 3.3 studio work w/o bonus missions

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Objects;
+
 @Controller
 public class HelloController {
 
@@ -27,7 +29,17 @@ public class HelloController {
     @ResponseBody
     public String helloForm() {
 
-        String html = "<form method='post'>"+"<input type='text' name='name' />"+"<input type='submit' value='Greet ME'/>"+"</form>";
+        String html = "<form method='post'>"
+                +"<input type='text' name='name' />"
+                +"<select name='language'>"
+                +"<option value='en'>English</option>"
+                +"<option value='fr'>French</option>"
+                +"<option value='de'>German</option>"
+                +"<option value='es'>Spanish</option>"
+                +"<option value='au'>Australian</option>"
+                +"</select>"
+                +"<input type='submit' value='Greet ME'/>"
+                +"</form>";
 
         return html;
 
@@ -35,10 +47,11 @@ public class HelloController {
 
     @RequestMapping(value = "hello", method = RequestMethod.POST)
     @ResponseBody
-    public String helloPost(HttpServletRequest request){
+    public String helloPost(HttpServletRequest request) {
         String name = request.getParameter("name");
+        String language = request.getParameter("language");
 
-        return "Hello " + name;
+        return HelloController.createMessage(name, language);
     }
 
     @RequestMapping(value = "/hello/{name}")
@@ -52,4 +65,23 @@ public class HelloController {
     public String goodbye() {
         return "Goodbye, frog";
     }
+
+    public static String createMessage(String name, String language){
+
+      String greeting = "Hello ";
+
+        if (language.equals("fr")) {
+            greeting = "Bonjour ";
+        } else if (language.equals("de")) {
+            greeting = "Guten Tag ";
+        } else if (language.equals("es")) {
+            greeting = "Hola ";
+        } else if (language.equals("au")) {
+            greeting = "G'day ";
+        }
+
+        return greeting + name;
+
+    }
+
 }
